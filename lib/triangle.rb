c3a15685c4b10aa761f6b4e45c989682d1568992
Uzor <uubabukoh@gmail.com>
2017-09-05 21:13:59 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  def initialize(sidea,sideb,sidec)
+    @sideA = sidea
+    @sideB = sideb
+    @sideC = sidec
+  end
+
+  def kind
+    sum1 = @sideA + @sideB
+    sum2 = @sideA + @sideC
+    sum3 = @sideB + @sideC
+
+    sumtest1 = sum1 > @sideC
+    sumtest2 = sum2 > @sideB
+    sumtest3 = sum3 > @sideA
+
+    if @sideA == 0 || @sideB == 0 || @sideC == 0
+      raise TriangleError
+    elsif !sumtest1 || !sumtest2 || !sumtest3
+      raise TriangleError
+    else
+      return :equilateral if (@sideA == @sideB) && (@sideB == @sideC)
+      return :isosceles if (@sideA == @sideB) || (@sideB == @sideC) || (@sideA == @sideC)
+      return :scalene
+    end
+  end
+
+end
+
+class TriangleError < StandardError
+ # triangle error code
 end
